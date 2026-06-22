320b5db22077d6a0761289f9234fc2240604b884
Vyacheslav <vyacheslav.e.201@gmail.com>
2026-06-22 19:43:35 +0900

    style: add css color variables

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app.tsx";
 import "./base.css";
+import "./variables.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
